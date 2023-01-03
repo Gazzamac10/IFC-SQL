bef8c752bc4f2c6e68f43b70640f6838b5040df5
mccarthyg <mccarthyg@UKLONLJSR01J3>
2023-01-03 12:13:06 +0000

    V1

---
+++ dashtrial.py
@@ -0,0 +1,37 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+
+
+#app = dash.Dash()
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/ThuwarakeshM/geting-started-with-plottly-dash/main/life_expectancy.csv"
+)
+
+print (df.columns)
+
+
+fig = px.scatter(
+    df,
+    x="GDP",
+    y="Life expectancy",
+    size="Population",
+    color="continent",
+    hover_name="Country",
+    log_x=True,
+    size_max=60,
+)
+
+fig.show()
+
+
+
+#app.layout = html.Div([dcc.Graph(id="life-exp-vs-gdp", figure=fig)])
+
+
+#if __name__ == "__main__":
+    #app.run_server(debug=True)
